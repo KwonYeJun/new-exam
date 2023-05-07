cbc5348614414e7ce63b474f8b2b23e3d0821585
gwonyejun <kyjuni1059@gmail.com>
2023-05-07 14:45:01 +0900

    Promise 이해를 못 하였다.

--- kwonyejun13.js
+++ kwonyejun13.js
@@ -76,7 +76,6 @@
 
 // * Q.1
 
-
 // function exampleOne(numberValue) {
 //   function integerCheck(data) {
 //     if (Number.isInteger(data) === true) {
@@ -127,57 +126,119 @@
 
 // * Q.2
 
+// function exampleOne(numberValue) {
+//   function integerCheck(data) {
+//     if (Number.isInteger(data) === true) {
+//       return Promise.resolve(data);
+//     } else {
+//       return Promise.reject("The input is not an integer.");
+//     }
+//   }
+
+
+
+//   // function integerCheck(data) {
+//   //   if (Number.isInteger(data) === true) {
+//   //     return new Promise((resolve, reject) => {
+//   //       resolve(data);
+//   //     });
+//   //   } else {
+//   //     return false;
+//   //   }
+//   // }
+
+//   integerCheck(numberValue)
+//     .then((data) => {
+//       return innerOne(data);
+//     })
+//     .then((data) => {
+//       return innerTwo(data);
+//     })
+//     .then((data) => {
+//       return innerThree(data);
+//     })
+//     .then((data) => {
+//       return innerFour(data);
+//     })
+//     .catch((error) => {
+//       console.log("catch문에서 잘 못됨");
+//     });
+
+//   // squence .2
+//   function innerOne(one) {
+//     return one + 1;
+//   }
+//   // sequence .3
+//   function innerTwo(two) {
+//     return two + 2;
+//   }
+//   // sequence.4
+//   function innerThree(three) {
+//     return three + 3;
+//   }
+//   // sequence.5
+//   function innerFour(four) {
+//     return four + 4;
+//   }
+
+//   // integerCheck(numberValue);
+//   // const first = innerOne(numberValue);
+//   // const second = innerTwo(first);
+//   // const third = innerThree(second);
+//   // const fourth = innerFour(third);
+//   // return fourth;
+// }
+// const result = exampleOne(10);
+// console.log(result);
+
+// ! gpt로 얻은 Promise 결과 값
 function exampleOne(numberValue) {
   function integerCheck(data) {
     if (Number.isInteger(data) === true) {
-      return new Promise((resolve, reject) => {
-        resolve(data);
-      });
+      return Promise.resolve(data);
     } else {
-      return false; 
+      return Promise.reject("입력값이 정수가 아닙니다.");
     }
   }
 
-  integerCheck(numberValue).then((data) => {
-    return innerOne(data);
-  })
-  .then((data) => {
+  return integerCheck(numberValue)
+    .then((data) => {
+      return innerOne(data);
+    })
+    .then((data) => {
       return innerTwo(data);
     })
-  .then((data) => {
-    return innerThree(data);
-  })
-  .then((data) => {
-    return innerFour(data);
-  })
-  .catch((error) => {
-    console.log('catch문에서 잘 못됨')
-  });
+    .then((data) => {
+      return innerThree(data);
+    })
+    .then((data) => {
+      return innerFour(data);
+    })
+    .catch((error) => {
+      console.log("에러가 발생했습니다.");
+    });
 
-  // squence .2
   function innerOne(one) {
     return one + 1;
   }
-  // sequence .3
+
   function innerTwo(two) {
     return two + 2;
   }
-  // sequence.4
+
   function innerThree(three) {
     return three + 3;
   }
-  // sequence.5
+
   function innerFour(four) {
     return four + 4;
   }
-
-  // integerCheck(numberValue);
-  // const first = innerOne(numberValue);
-  // const second = innerTwo(first);
-  // const third = innerThree(second);
-  // const fourth = innerFour(third);
-  // return fourth;
-
 }
-const result = exampleOne(10);
-console.log(result);
+
+exampleOne(10)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
