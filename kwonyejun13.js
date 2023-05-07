8f625029f14aeebb91913206217c5c2375404b0e
gwonyejun <kyjuni1059@gmail.com>
2023-05-07 14:49:53 +0900

    Promise 결과값 도출실패

--- kwonyejun13.js
+++ kwonyejun13.js
@@ -126,80 +126,26 @@
 
 // * Q.2
 
-// function exampleOne(numberValue) {
-//   function integerCheck(data) {
-//     if (Number.isInteger(data) === true) {
-//       return Promise.resolve(data);
-//     } else {
-//       return Promise.reject("The input is not an integer.");
-//     }
-//   }
-
-
-
-//   // function integerCheck(data) {
-//   //   if (Number.isInteger(data) === true) {
-//   //     return new Promise((resolve, reject) => {
-//   //       resolve(data);
-//   //     });
-//   //   } else {
-//   //     return false;
-//   //   }
-//   // }
-
-//   integerCheck(numberValue)
-//     .then((data) => {
-//       return innerOne(data);
-//     })
-//     .then((data) => {
-//       return innerTwo(data);
-//     })
-//     .then((data) => {
-//       return innerThree(data);
-//     })
-//     .then((data) => {
-//       return innerFour(data);
-//     })
-//     .catch((error) => {
-//       console.log("catch문에서 잘 못됨");
-//     });
-
-//   // squence .2
-//   function innerOne(one) {
-//     return one + 1;
-//   }
-//   // sequence .3
-//   function innerTwo(two) {
-//     return two + 2;
-//   }
-//   // sequence.4
-//   function innerThree(three) {
-//     return three + 3;
-//   }
-//   // sequence.5
-//   function innerFour(four) {
-//     return four + 4;
-//   }
-
-//   // integerCheck(numberValue);
-//   // const first = innerOne(numberValue);
-//   // const second = innerTwo(first);
-//   // const third = innerThree(second);
-//   // const fourth = innerFour(third);
-//   // return fourth;
-// }
-// const result = exampleOne(10);
-// console.log(result);
-
-// ! gpt로 얻은 Promise 결과 값
 function exampleOne(numberValue) {
   function integerCheck(data) {
     if (Number.isInteger(data) === true) {
       return Promise.resolve(data);
     } else {
-      return Promise.reject("입력값이 정수가 아닙니다.");
+      return Promise.reject("The input is not an integer.");
     }
   }
+
+
+
+  // function integerCheck(data) {
+  //   if (Number.isInteger(data) === true) {
+  //     return new Promise((resolve, reject) => {
+  //       resolve(data);
+  //     });
+  //   } else {
+  //     return false;
+  //   }
+  // }
 
   return integerCheck(numberValue)
     .then((data) => {
@@ -215,26 +161,80 @@ function exampleOne(numberValue) {
       return innerFour(data);
     })
     .catch((error) => {
-      console.log("에러가 발생했습니다.");
+      console.log("catch문에서 잘 못됨");
     });
 
+  // squence .2
   function innerOne(one) {
     return one + 1;
   }
-
+  // sequence .3
   function innerTwo(two) {
     return two + 2;
   }
-
+  // sequence.4
   function innerThree(three) {
     return three + 3;
   }
-
+  // sequence.5
   function innerFour(four) {
     return four + 4;
   }
-}
 
+  // integerCheck(numberValue);
+  // const first = innerOne(numberValue);
+  // const second = innerTwo(first);
+  // const third = innerThree(second);
+  // const fourth = innerFour(third);
+  // return fourth;
+}
+// const result = exampleOne(10);
+// console.log(result);
+
+// ! gpt로 얻은 Promise 결과 값
+// function exampleOne(numberValue) {
+//   function integerCheck(data) {
+//     if (Number.isInteger(data) === true) {
+//       return Promise.resolve(data);
+//     } else {
+//       return Promise.reject("입력값이 정수가 아닙니다.");
+//     }
+//   }
+
+//   return integerCheck(numberValue)
+//     .then((data) => {
+//       return innerOne(data);
+//     })
+//     .then((data) => {
+//       return innerTwo(data);
+//     })
+//     .then((data) => {
+//       return innerThree(data);
+//     })
+//     .then((data) => {
+//       return innerFour(data);
+//     })
+//     .catch((error) => {
+//       console.log("에러가 발생했습니다.");
+//     });
+
+//   function innerOne(one) {
+//     return one + 1;
+//   }
+
+//   function innerTwo(two) {
+//     return two + 2;
+//   }
+
+//   function innerThree(three) {
+//     return three + 3;
+//   }
+
+//   function innerFour(four) {
+//     return four + 4;
+//   }
+// }
+// ! 이 부분이 잘 이해가 안 간다. 이렇게 작성을 했을 때는 값이 나오는지 아직 이해가 안 간다.
 exampleOne(10)
   .then((result) => {
     console.log(result);
